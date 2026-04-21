0d214db114251714322c5b589cea86727b6f0022
Barath <barath101097@gmail.com>
2026-04-21 10:14:27 +0530

    Initial React project commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Selfintro from './Selfintro'
+export default function App() {
+  return (
+    <div>
+      
+        <Selfintro />
+      
+    </div>
+  )
+}
---
+++ src/Selfintro.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./App.css"
+export default function Selfintro() {
+  return (
+    <div>
+    <section className="intro">
+      <h1>Hello, I’m <span className="highlight">JayaBarath</span></h1>
+      <p>
+        A <span className="highlight">full‑stack developer‑in‑training</span> from India,
+        passionate about <span className="highlight">creative themed layouts</span>, 
+        <span className="highlight">async JavaScript</span>, and seamless UI integration.
+      </p>
+      <p>
+        I love building <span className="highlight">responsive dashboards</span> with 
+        clean, modular code and exploring playful problem‑solving challenges.
+      </p>
+        <div className="spacer">
+    <span>Html</span>
+    <span>Css</span> 
+    
+    <span>JavaScript</span>
+    <span>React</span>
+  </div>
+    </section>
+  
+
+
+    </div>
+  )
+}
