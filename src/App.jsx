b45225565a0dc08bf04a2899c9cb7565c9e7d5b3
Barath <barath101097@gmail.com>
2026-05-18 07:53:01 +0530

    final page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
