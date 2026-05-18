40cad6486c7a6ccc39c5d516d46f4c88d4868cb4
Barath <barath101097@gmail.com>
2026-05-18 07:56:12 +0530

    final page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import Gallery from "./pages/Gallery";
+import Layout from "./Components/Layout";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Contact from "./Pages/Contact";
+import Gallery from "./Pages/Gallery";
 
 function App() {
   return (
--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,4 +1,4 @@
-import ServiceCard from "../components/ServiceCard";
+import ServiceCard from "../Components/ServiceCard";
 import React from "react";
 
 const services = [
